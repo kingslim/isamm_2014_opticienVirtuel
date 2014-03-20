f13b37daa1396f0e8312b78e9df3a2be86b2f9b5
rafrafi <rafrafiyassine@gmail.com>
2014-03-20 19:10:06 +0100

    opencv et opengl integré ensemble
    le dossier "opencv et opengl"  contient le projet qui permet de detecter
    un visage et de le plaquer comme texture sur un plan et de faire le
    rendu en mode perspective.

---
+++ opencvapp detect face from video/ConsoleApplication1/main.cpp
@@ -0,0 +1,16 @@
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	IplImage* img = cvLoadImage("image.jpg"); //change the name (image.jpg) according to your Image filename.
+	cvNamedWindow("Example1", CV_WINDOW_NORMAL);
+	cvShowImage("Example1", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("Example1");
+	return 0;
+
+}
